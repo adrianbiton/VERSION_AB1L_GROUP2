3f8e47c3cf4844bdf2f28ea066c3dec0e995782a
fredericke2 <frederick.scbr@gmail.com>
2013-03-05 11:05:31 +0800

    Update add_blog.php
    UI

--- add_blog.php
+++ add_blog.php
@@ -1,53 +1,103 @@
 <?php
 	session_start();
-	
+
     $pgsql_conn = pg_connect("dbname=postgres host=localhost user=postgres password=12345");
 ?>
 
-
-<html>
+<html lang="en-US">
 	<head>
 		<title>Add Blog Entry</title>
-		<!-- modernizr enables HTML5 elements and feature detects -->
-		<script type="text/javascript" src="js/modernizr-1.5.min.js"></script>	
+		<link href="CSS/style.css" rel="stylesheet" type="text/css">
+		<!--[if IE 6]>
+		<script type="text/ecmascript" src="js/html5.js"></script>
+		<![endif]-->
 	</head>
-	
 	<body>
-	
-	<form enctype = "multipart/form-data" name = "add_blog" action = "add_blog.php" method = "post">
-		
-		<!--div for left part of the page-->
-		<div id = "left_cblog">
-			<!--div for BLOG_TITLE-->
-			<div name = "blog_title">
-				Blog Title:<input type = "text" name = "title" id = "title" placeholder = "Title"/>
-			</div>
-			
-			<!--div for BLOG_PIC-->
-			<div name = "blog_pic">
-				Blog Image:<input type = "file" name = "uploaded" id = "pic" size = "50" />
-			</div>
-			
-			<!--div for image_CAPTION-->
-			<div name = "image_caption">
-				Image Caption:<textarea name = "caption" cols = 40 rows = 10 placeholder = "Type image caption here"></textarea>
+
+		<div id="wrap">
+			<header id="mainheader">
+				<h1>Site <span>Name</span></h1>
+			</header>
+			<nav id="topnav">
+				<ul>
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="view_profile.php">Profile</a></li>
+					<li><a href="view_blog.php">Blogs</a></li>
+					<li><a href="logout.php">Logout</a></li>
+					<li><form name = "search" method = "post" action = "search_result.php">
+						<label for = "searchme">Search: </label><input type = "search" id = "searchme" name = "searchme"/>
+					</form></li>
+				</ul>
+			</nav>
+			<div id="maincontent">
+				<section id="leftcontent" class="normalpage">
+					<section id="leftcontents">
+						<header id="mainheading">
+							<h2>Publish Journal</h2>
+						</header>
+						<div id="contentwrap">
+							<form enctype = "multipart/form-data" name = "add_blog" action = "add_blog.php" method = "post">
+								<article class="postpre">
+									<header>
+										<h3>Blog Title:</h3>
+									</header>
+									<input type = "text" name = "title" id = "title" placeholder = "Title"/>
+								</article>
+								<div class="postbtm"></div>
+								<article class="postpre">
+									<header>
+										<h3>Blog Image:</h3>
+										</header>
+									<input type = "file" name = "uploaded" id = "pic" size = "50" />
+								</article>
+								<div class="postbtm"></div>
+								<article class="postpre">
+									<header>
+										<h3>Image Caption:</h3>
+									</header>
+									<textarea name = "caption" cols = 40 rows = 3 placeholder = "Type image caption here"></textarea>
+								</article>
+								<div class="postbtm"></div>
+								<article class="postpre">
+									<header>
+										<h3>Blog Entry:</h3>
+									</header>
+									<textarea name = "text" cols = 40 rows = 10 required placeholder = "Type your thoughts here :)"></textarea>
+								</article>
+								<div class="postbtm"></div>
+								<article class="postpre">
+									<header>
+										<h3>Publish</h3>
+									</header>
+									<input type = "submit" value = "Publish!" name = "add_blog" />
+								</article>
+								<div class="postbtm"></div>
+							</form>
+						</div>
+					</section>
+				</section>
+				<section id="sidebar">
+					<img src="images/default.png" height="10%" width="30%"/>
+					<h2 title="Categories">Username</h2>
+						<div class="sb-c">
+							<p class="testimonial">
+								<img src="images/star.png" height="5%" width="15%"/>
+								<p>star count</p>
+							</p>
+						</div>
+					<div class="sb-c">
+						<a href="view_blog.php">My Blogs</a>
+						<a href="view_profile.php">My Profile</a>
+					</div>
+				</section>
+				<div class="clear"></div>
 			</div>
 		</div>
-		
-		<!--div for right part--CONTAINS THE BLOG ENTRY-->
-		<div id = "blog_entry">
-			<!--div for BLOG_TEXT-->
-			<div name = "blog_text">
-				Blog Entry:<textarea name = "text" cols 50 rows = 20 required placeholder = "Type your thoughts here :)"></textarea>
-			</div>
-		</div>
-		
-		<!--BUTTON HERE-->
-		<input type = "submit" value = "Publish!" name = "add_blog" />
-	</form>
-	
-	
-	<?php
+		<footer>
+			<p>2013 &copy; Online Diary| All Rights Reserved|Partial Fulfillment of Requirements In CMSC 128</a></p>
+
+		</footer>
+		<?php
 		//FUNCTION FOR CHECKING FILE EXTENSION
 		 function findexts ($filename) 
 		 { 
@@ -57,10 +107,10 @@
 		 $exts = $exts[$n]; 
 		 return $exts; 
 		 } 
-		 
+
 		if(isset($_POST['add_blog'])){
-			
-			
+
+
 			//UPLOADS IMAGE FILES
 			if($_FILES['uploaded']['name'] != null){
 				//This applies the function to our file  
@@ -76,7 +126,7 @@
 				move_uploaded_file($_FILES['uploaded']['tmp_name'], $target);
 			}
 			//END-UPLOAD
-			
+
 			//SAVE THE BLOG ENTRY IN A FILE
 			$ran3 = rand();
 			$filename = $ran3.".txt";
@@ -87,7 +137,7 @@
 			print "Blog Published!<br/>";
 			fclose($filehandle);
 			//END-SAVE-BLOG-ENTRY
-			
+
 			//check values first
 			if($_POST['title'] != null)
 				$titleV = $_POST['title'];
@@ -97,14 +147,14 @@
 				$captionV = $_POST['caption'];
 			else
 				$captionV = null;
-			
+
 			//real query
 			$query = "INSERT INTO blogs VALUES('".$ran3."', now(), '".$ran2.$ext."', '". $titleV."', '". $captionV."', '".$_SESSION['uname']."');";
-			
+
 			echo $query;
 			$result = pg_exec($pgsql_conn, $query) or die('Query failed: ' . pg_last_error());
 		}
-		
+
 	?>
 	</body>
 </html>
